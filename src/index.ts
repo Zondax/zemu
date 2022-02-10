db0e6a1281b7b31fe62df4691e03bec0de853be0
Emmanuel <emmanuelm41@gmail.com>
2022-02-10 12:17:43 -0300

    add startTimeout configuration value on startOptions

--- src/index.ts
+++ src/index.ts
@@ -51,7 +51,8 @@ export const DEFAULT_START_OPTIONS = {
   custom: '',
   startDelay: DEFAULT_START_DELAY,
   pressDelay: DEFAULT_KEY_DELAY,
-  startText: 'Ready'
+  startText: 'Ready',
+  startTimeout: 5000
 }
 
 export class StartOptions {
@@ -61,6 +62,7 @@ export class StartOptions {
   custom = ''
   startDelay = DEFAULT_START_DELAY
   startText = 'Ready'
+  startTimeout = 5000
 }
 
 export interface Snapshot {
@@ -229,7 +231,7 @@ export default class Zemu {
       this.log(`Get initial snapshot`)
 
       // Captures main screen
-      await this.waitForText(this.startOptions.startText)
+      await this.waitForText(this.startOptions.startText, this.startOptions.startTimeout)
       this.mainMenuSnapshot = await this.snapshot()
     } catch (e) {
       this.log(`[ZEMU] ${e}`)
